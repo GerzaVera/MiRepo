900d006325cda339dc1412e8cdc05144dc28a7ba
irving vera <irvingvera@MacBook-Pro-de-irving.local>
2024-04-03 23:03:25 -0600

    se agregararo galeria mexico

---
+++ js/script.js
@@ -0,0 +1,83 @@
+//MONTERREY
+
+var like = document.querySelector("#like");
+
+var contador = document.querySelector("#conteo");
+var contador_1 = document.querySelector("#conteo_1");
+var contador_2 = document.querySelector("#conteo_2");
+var contador_3 = document.querySelector("#conteo_3");
+var contador_4 = document.querySelector("#conteo_4");
+var contador_5 = document.querySelector("#conteo_5");
+
+contador = parseInt(contador);
+contador_1 =parseInt(contador_1);
+contador_2 =parseInt(contador_2);
+contador_3 =parseInt(contador_3);
+contador_4 =parseInt(contador_4);
+contador_5 =parseInt(contador_5);
+
+contador = 0;
+contador_1 = 0;
+contador_2 = 0;
+contador_3 = 0;
+contador_4 = 0;
+contador_5 = 0;
+
+
+
+
+
+
+//Monterrey-cerro de la silla
+function cerroSilla(){
+	
+	contador++;
+	
+	conteo.innerHTML=contador;
+
+}
+
+//Monterrey-vista de noche
+function monterreyNoche(){
+	
+	contador_1++;
+	
+	conteo_1.innerHTML=contador_1;
+
+}
+
+//parque del Bosque
+function bosquePark(){
+	
+	contador_2++;
+	
+	conteo_2.innerHTML=contador_2;
+
+}
+//museo del Acero
+function museoAcero(){
+	
+	contador_3++;
+	
+	conteo_3.innerHTML=contador_3;
+
+}
+
+//cañon de la huasteca
+function canonHuasteca(){
+	
+	contador_4++;
+	
+	conteo_4.innerHTML=contador_4;
+
+}
+//grutas de garcia
+function grutasGarcia(){
+	
+	contador_5++;
+	
+	conteo_5.innerHTML=contador_5;
+
+}
+//FIN DE MONTERREY
+
